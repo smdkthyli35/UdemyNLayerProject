e009398a3b03418bbcb2b064d08806fa7db8e18f
zeljkosamed <smdkthyli@gmail.com>
2022-03-12 18:24:31 +0300

    PersonController ve action metodları oluşturuldu.

---
+++ UdemyNLayerProject.API/Controllers/PersonsController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyNLayerProject.API.DTOs;
+using UdemyNLayerProject.Core.Models;
+using UdemyNLayerProject.Core.Services;
+
+namespace UdemyNLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+        private readonly IMapper _mapper;
+
+        public PersonsController(IPersonService personService, IMapper mapper)
+        {
+            _personService = personService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var people = await _personService.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<PersonDto>>(people));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var person = await _personService.GetByIdAsync(id);
+            return Ok(_mapper.Map<PersonDto>(person));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(PersonDto personDto)
+        {
+            var person = _mapper.Map<Person>(personDto);
+            var newPerson = await _personService.AddAsync(person);
+            return Created(string.Empty, _mapper.Map<PersonDto>(newPerson));
+        }
+
+        [HttpPut]
+        public IActionResult Update(PersonDto personDto)
+        {
+            var person = _mapper.Map<Person>(personDto);
+            var updatedPerson = _personService.Update(person);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var person = _personService.GetByIdAsync(id).Result;
+            _personService.Remove(person);
+            return NoContent();
+        }
+    }
+}
