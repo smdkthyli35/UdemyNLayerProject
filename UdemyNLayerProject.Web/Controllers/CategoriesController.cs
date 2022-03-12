30d5ca067667741b0759da69a66b1dbc0a9a227d
zeljkosamed <smdkthyli@gmail.com>
2022-03-12 21:48:46 +0300

    CategoriesController index sayfası oluşturuldu ve kategoriler listelendi.

---
+++ UdemyNLayerProject.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Services;
+using UdemyNLayerProject.Web.DTOs;
+
+namespace UdemyNLayerProject.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return View(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+        }
+    }
+}
