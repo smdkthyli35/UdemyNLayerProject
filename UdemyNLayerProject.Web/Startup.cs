ee065cbd72ca95344ccad9986c32fbbf6eef2849
zeljkosamed <smdkthyli@gmail.com>
2022-03-12 21:31:45 +0300

    Automapper kütüphanesi yüklendi ve Dtolar oluşturuldu.

--- UdemyNLayerProject.Web/Startup.cs
+++ UdemyNLayerProject.Web/Startup.cs
@@ -31,6 +31,7 @@ namespace UdemyNLayerProject.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddScoped(typeof(IService<>), typeof(Service.Services.Service<>));
